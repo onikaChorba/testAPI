ce024ef9a0902bae83a9c49f832c761d2b3bc3a9
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-03-20 17:44:00 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@component/styles/globals.css'
+import '@component/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {

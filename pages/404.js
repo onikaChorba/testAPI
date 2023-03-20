ecfe8d415cfdac209a1f1a2bef115b41ac017412
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-03-20 20:15:43 +0200

    feat: add folder components, add 404.js, remote some files; style: delate style

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+export default function Error() {
+  return (
+    <>
+      <h1>404</h1>
+      <h2>Something is going wrong...</h2>
+      <img src="/cat.png" style={{ width: "70px" }} />
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@component/styles/globals.scss'
+import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
-
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+ ) 
 }

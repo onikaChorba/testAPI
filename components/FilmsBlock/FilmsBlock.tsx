3dc48ea76d54b253a608793bc0ab37430fcc768b
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-03-26 17:33:01 +0300

    feat: add type in component films; style: add style in filmsBlock

--- components/FilmsBlock/FilmsBlock.tsx
+++ components/FilmsBlock/FilmsBlock.tsx
@@ -1,19 +1,30 @@
+import styles from './FilmsBlock.module.scss';
+import { filmsType } from "@component/tipes";
+import { FC } from 'react';
 
-export const FilmsBlock = ({films}:any)=>{
+
+type filmsInfoProps = {
+  films: Array <filmsType>
+}
+
+export const FilmsBlock:FC<filmsInfoProps> = ({films})=>{
   return (
-    <div>{films.map((film:any )=> (
-      <div key={film.score}>
-        <div >{film.show.name}</div>
-        <p>Status : {film.show.status}</p>
-        <p> Premired: {film.show.premiered} Ended: {film.show.ended}</p>
-        <p> Show: time - {film.show.schedule.time}, days: {film.show.schedule.days}</p>
-        <div> Language : {film.show.language}</div>
-        <p> Country: {film.show.network?.country.name}</p>
-        <div> Genders : {film.show.genres}</div>
-        <a href={film.show.officialSite}> link site:</a><span> {film.show.officialSite}</span>
-        <a href={film.show.url}> Open site: <span>{film.show.url}</span></a>
-        <img src={film.show.image.original} alt={film.show.name} width={200}/>
-        {film.show.summary}
+    <div className={styles.filmsBlock}>{films.map((film, number )=> (
+      <div key={number.toString()} className={styles.filmBlock}>
+        <img src={film.show.image.original} alt={film.show.name} className={styles.filmBlock__img}/>
+        <div className={styles.filmBlock__info}>
+          <div className={styles.filmInfo__name}>{film.show.name}</div>
+          <p>Status : {film.show.status}</p>
+          <p> Premired: {film.show.premiered} Ended: {film.show.ended}</p>
+          <p> Show: time - {film.show.schedule.time}, days: {film.show.schedule.days}</p>
+          <p> Language : {film.show.language}</p>
+          <p> Country: {film.show.network?.country.name}</p>
+          <p> Genders : {film.show.genres}</p>
+          <div className={styles.filmInfo__buttons}>
+            <button className={styles.filmInfo__button}><a href={film.show.officialSite} target="_blank" > Show more info</a></button>
+            <button className={styles.filmInfo__button}><a href={film.show.url} target="_blank"> Open site</a></button>
+          </div>
+        </div>
       </div>
     ))}</div>
   )
